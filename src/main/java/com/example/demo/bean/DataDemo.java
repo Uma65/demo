95154b34cc774a6f681bfed7380e5d1be0cfac13
umadevi <umadevinangana@gmail.com>
2018-10-11 06:54:00 +0530

    done

---
+++ src/main/java/com/example/demo/bean/DataDemo.java
@@ -0,0 +1,19 @@
+package com.example.demo.bean;
+
+public class DataDemo {
+private Integer id;
+private String child_name;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getChild_name() {
+	return child_name;
+}
+public void setChild_name(String child_name) {
+	this.child_name = child_name;
+}
+
+}
